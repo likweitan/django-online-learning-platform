83ca816b736ca41e7fa7090f63bdb64cc1ae7e30
likweitan <likweitan@gmail.com>
2021-04-30 13:58:53 +0800

    Migrated to PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,11 +80,31 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'rpvcbzns',
+
+        'USER': 'rpvcbzns',
+
+        'PASSWORD': 'bxmLucZRCvn_DUiSvn0Xhruy1tQqbbEQ',
+
+        'HOST': 'rosie.db.elephantsql.com',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
